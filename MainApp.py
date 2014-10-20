d7371579a22bb96c498dc609d8926d4e457ec412
JefferShell <1876993774@qq.com>
2014-10-20 19:55:38 +0800

    可以登录
    Signed-off-by: JefferShell <1876993774@qq.com>

--- MainApp.py
+++ MainApp.py
@@ -4,37 +4,46 @@ import datetime
 from web import form
 from models import getUserByUserName
 # from setting import urls
-
+web.config.debug = False
 urls=(
       '/', 'MainPage',
       '/Login', 'Login',
+      '/Logout', 'Logout',
       '/(.*)', 'Page404'
       )
-app = web.application(urls,globals())
+app = web.application(urls,globals()) 
+#设置静态文件的目录
+web.template.Template.globals['static'] = '/static'
 render = web.template.render('templates',base='base')
-session = web.session.Session(app, web.session.DiskStore('sessions'),initializer={'login':0,})
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'count': 0})  
+web.config._session = session   
+tempInfo = {}
 class MainPage():
     def GET(self):
         info = {}
+        print session.count
         print 'MainPage'
         ip = web.ctx  
 #         print ip
         curTime = datetime.datetime.now().strftime("%Y-%m-%d  %H:%M:%S")
-        info["ip"] = ip.ip
-        info["curTime"] = curTime
-        return render.MainPage(info)
+        tempInfo["ip"] = ip.ip
+        tempInfo["curTime"] = curTime
+        if logged() :
+            tempInfo["loginInfo"] = "您已登录!"
+        else:
+            tempInfo["loginInfo"] = "未登录！"
+        return render.MainPage(tempInfo)
 class Page404:
     def GET(self,name):
         print "404"
         errorStr = name,"页面没找到，请处理"
         return render.Page404(name)
 class Login:
-    print "Login--"
     loginForm=form.Form(form.Textbox('username',description=u'用户名'),
                         form.Password("password",description=u"密码"),
                         form.Button(u"马上登陆", type="submit", description="submit"),)
     def GET(self):
-        if logged(session):
+        if logged():
             return 'you are logged'
         else:
             return render.LoginPage(self.loginForm)
@@ -50,18 +59,23 @@ class Login:
             return render.login(self.LoginForm,errorInfo)
         else:
             if password==rslist[0].pwd:                
-                session.login=1
+                session.count=1
                 #session.login222=1
 #                 return  '登陆成功,欢迎你:'+username+':'+str(session.login)
-                return '登录成功，欢迎你*****',username,':',str(session.login)
+#                 return render.MainPage(tempInfo)
+                raise web.seeother("/",tempInfo)
             else:
                 return render.Login(self.loginForm,'用户名及密码不匹配')
+class Logout():
+    def GET(self):
+        print "logout"
+        session.kill()
+        raise web.seeother("/", tempInfo)
 #
-def logged(session):
-    return False
-#     if session.login == 1:
-#         return True
-#     else:
-#         return False
+def logged():
+    if session.count == 1:
+        return True
+    else:
+        return False
 if __name__ == '__main__':
     app.run()
